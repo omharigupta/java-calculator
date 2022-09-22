6ac5b3d657147f96855483242ac10357b549158b
omharigupta <106465453+omharigupta@users.noreply.github.com>
2022-09-22 15:32:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int num1,num2;
+        String= operator;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        num1=s.nextInt();
+        num2=s.nextInt();
+        System.out.println("Enter operator : ");
+        operator=s.nextLine();
+        if (operator == "Add"){
+            System.out.println(x+y);}        
+        else if (operator == "Subtract"){
+           System.out.println(x-y);}
+        else if (operator == "Multiply"){
+           System.out.println(x*y);}
+        else{
+            System.out.println(x/y);}
+        }
+}
+}
